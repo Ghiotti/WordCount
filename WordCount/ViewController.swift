7b943b8b58deba98d228d948ea40c549b337c708
Franco Ghiotti <fran.ghiott@gmail.com>
2021-04-17 12:30:50 +0200

    Initial Commit

---
+++ WordCount/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordCount
+//
+//  Created by Franco Ghiotti on 17/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
